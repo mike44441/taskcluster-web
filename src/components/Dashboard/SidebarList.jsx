32cd08eb516d426fe5d6f7ccc93feefd9f065bd7
M Christensen <michael.christensen@wsu.edu>
2018-09-27 10:04:31 -0700

    Requested Changes
    Removing purges caches icon and replacing caches
    
    Replacing Caches with Purge Caches and changing Purges Caches icon to the BackupRestoreIcon.

--- src/components/Dashboard/SidebarList.jsx
+++ src/components/Dashboard/SidebarList.jsx
@@ -20,7 +20,6 @@ import FileTreeIcon from 'mdi-react/FileTreeIcon';
 import WebhookIcon from 'mdi-react/WebhookIcon';
 import KeyIcon from 'mdi-react/KeyIcon';
 import PulseIcon from 'mdi-react/PulseIcon';
-import AnimationOutlineIcon from 'mdi-react/AnimationOutlineIcon';
 import SidebarListGroup from './SidebarListGroup';
 import SidebarListItem from './SidebarListItem';
 
@@ -78,10 +77,6 @@ export default class SidebarList extends Component {
 
         <SidebarListItem to="/pulse-messages" icon={<PulseIcon />}>
           Pulse Messages
-        </SidebarListItem>
-
-        <SidebarListItem to="/purge-caches" icon={<AnimationOutlineIcon />}>
-          Purge Cache
         </SidebarListItem>
 
         <SidebarListItem to="/expansions" icon={<ArrowExpandVerticalIcon />}>
